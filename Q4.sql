f865c6abd3030376c3722b4b445a923c38ba8f54
Ersanios <ersanios.yakoub@ynov.com>
2026-02-26 22:59:42 +0100

    4e fochier

---
+++ Q4.sql
@@ -0,0 +1,21 @@
+
+SELECT COUNT(*) AS total_vehicules
+FROM vehicule;
+
+SELECT ville,
+       COUNT(*) AS nombre_vehicules
+FROM vehicule
+GROUP BY ville
+ORDER BY nombre_vehicules DESC;
+
+SELECT AVG(autonomie_km) AS autonomie_moyenne
+FROM vehicule;
+
+SELECT c.id_client,
+       c.nom,
+       c.prenom,
+       COUNT(l.id_location) AS nombre_locations
+FROM client c
+JOIN location l ON c.id_client = l.id_client
+GROUP BY c.id_client, c.nom, c.prenom
+ORDER BY nombre_locations DESC;
